551768e760ef8b98a89eb753e4bb619adfd47f58
dylancarfley <56799276+dylancarfley@users.noreply.github.com>
2020-03-31 17:42:34 -0400

    Update cardClass.java
    made setters, getters, constructors and tostring for a basic card class

--- FlashCards/src/flashcards/cardClass.java
+++ FlashCards/src/flashcards/cardClass.java
@@ -10,5 +10,47 @@ package flashcards;
  * @author DylanCarfley
  */
 public class cardClass {
+    private int id;
+    private int question;
+    private int answer;
+
+    public cardClass() {
+    }
+
+    public cardClass(int id, int question, int answer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(int question) {
+        this.question = question;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "cardClass{" + "id=" + id + ", question=" + question + ", answer=" + answer + '}';
+    }
+    
     
 }
